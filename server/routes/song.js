93ec9ec4b38d3131d040b1e36baea38c14f91a51
shreyasmn <mnshreyasiyengar@gmail.com>
2023-11-18 15:23:19 +0530

    song.js is working but the passport.authenticate is not added for now will add it later

--- server/routes/song.js
+++ server/routes/song.js
@@ -1,22 +1,48 @@
 const express = require("express");
 const router = express.Router();
 const passport = require("passport");
-const mysql = require("mysql");
+const mysql = require("mysql2");
 const util = require("util");
+
+const db_name = "har";
+
+//put the authenticate using token passort back in each api call
 
 const db = mysql.createPool({
   host: "localhost",
   user: "root",
-  password: process.env.MYSQL_PASSWORD,
-  database: "harmony_hub",
+  password: process.env.mySQLPassword,
+  database: db_name,
   port: 3306 // Default MySQL port is 3306
+}).promise();
+
+db.getConnection().then(connection => {
+console.log('Connected to the database');
+connection.release();
+})
+.catch(error => {
+console.error('Error connecting to the database:', error.message);
 });
 
-const dbQuery = util.promisify(db.query).bind(db);
+async function dbQuery(query, params) {
+  try {
+    const [rows, fields] = await db.query(query, [params]);
+
+    if (rows.length > 0) {
+      const user = rows[0];
+      return user;
+    } else {
+      return null;
+    }
+  } catch (error) {
+    console.error('Error querying the database:', error.message);
+    throw error;
+  }
+}
+  
 
 router.post(
   "/create",
-  passport.authenticate("jwt", { session: false }),
   async (req, res) => {
     const { name, thumbnail, track } = req.body;
     if (!name || !thumbnail || !track) {
@@ -24,13 +50,15 @@ router.post(
         .status(301)
         .json({ err: "Insufficient details to create song." });
     }
-    const artist = req.user._id;
+    // const artist = req.user._id;
+    const artist = "abc";
     const songDetails = { name, thumbnail, track, artist };
     const insertQuery = "INSERT INTO songs SET ?";
     try {
       const result = await dbQuery(insertQuery, songDetails);
-      const createdSong = { id: result.insertId, ...songDetails };
-      return res.status(200).json(createdSong);
+      // const createdSong = { id: result.insertId, ...songDetails };
+      // return res.status(200).json(createdSong);
+      return res.status(200).json({ data: "Song successfully created" });
     } catch (error) {
       return res.status(500).json({ err: "Error creating song" });
     }
@@ -39,9 +67,9 @@ router.post(
 
 router.get(
   "/get/mysongs",
-  passport.authenticate("jwt", { session: false }),
   async (req, res) => {
-    const artistId = req.user._id;
+    // const artistId = req.user._id;
+    const artistId = "abc";
     const query = "SELECT * FROM songs WHERE artist = ?";
     try {
       const result = await dbQuery(query, artistId);
@@ -54,7 +82,6 @@ router.get(
 
 router.get(
   "/get/artist/:artistId",
-  passport.authenticate("jwt", { session: false }),
   async (req, res) => {
     const { artistId } = req.params;
     const query = "SELECT * FROM songs WHERE artist = ?";
--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ const JwtStrategy = require('passport-jwt').Strategy,
     ExtractJwt = require('passport-jwt').ExtractJwt;
 const passport = require('passport')
 const authRoutes = require('./routes/auth')
+const songRoutes = require('./routes/song')
 
 const app = express()
 app.use(cors())
@@ -37,24 +38,38 @@ console.log('Connected to the database');
 
 // passport.use(new JwtStrategy(opts, (jwt_payload, done) => {
 //     // Assuming you have a MySQL connection named 'db'
-//     db.query('SELECT * FROM users WHERE id = ?', [jwt_payload.sub], (err, rows) => {
-//         if (err) {
-//             return done(err, false);
-//         }
+//     // db.query('SELECT * FROM users WHERE id = ?', [jwt_payload.sub], (err, rows) => {
+//     //     if (err) {
+//     //         return done(err, false);
+//     //     }
 
-//         if (rows.length > 0) {
+//     //     if (rows.length > 0) {
+//     //         const user = rows[0];
+//     //         return done(null, user);
+//     //     } else {
+//     //         return done(null, false);
+//     //         // or you could create a new account
+//     //     }
+//     // });
+//     async function getUser() {
+//         try {
+//           const [rows, fields] = await db.query('SELECT email FROM demousers WHERE email = ?', [email]);
+      
+//           if (rows.length > 0) {
 //             const user = rows[0];
 //             return done(null, user);
-//         } else {
+//           } else {
 //             return done(null, false);
-//             // or you could create a new account
+//           }
+//         } catch (error) {
+//           console.error('Error querying the database:', error.message);
+//           return done(error, false);
 //         }
-//     });
+//       }
 // }));
   
 app.get('/', (req, res) => {
     const sql = 'select * from albums';
-    // const sql = 'show tables';
     db.query(sql, (err, data)=>{
         if(err) return res.json(err);
         return res.json(data);
@@ -62,6 +77,8 @@ app.get('/', (req, res) => {
 })
 
 app.use("/auth", authRoutes);
+
+app.use("/song", songRoutes);
 
 app.listen(port, ()=>{
     console.log('listening to port', port)
